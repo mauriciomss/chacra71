d2a7c23ac1c78bf7dfc02c71399e08f0e92333c5
mauriciomss <mauriciomss@gmail.com>
2023-11-13 13:58:59 -0300

    commit 13-11-2023 13:58:59

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -11,8 +11,32 @@ class Home extends BaseController
         return view('Home', $data);
     }
 
-    public function luminaria() //: string
+    public function coordenadas() //: string
     {
-        echo "luminaria";
+        $luminaria = [
+            [
+                "problema" => "No anda",
+                "direccion" => "Miño 3654",
+                "imagen" => "http://localhost/chacra71/assets/dist/img/logo.png",
+                "latitud" => -27.381221988814545,
+                "longitud" => -55.916646268651796,
+                "icono" => "lighthouse-no.png",
+            ],
+            [
+                "problema" => "No anda",
+                "direccion" => "Luchessi 3980",
+                "imagen" => "http://localhost/chacra71/assets/dist/img/logo.png",
+                "latitud" => -27.38067419397892,
+                "longitud" => -55.91704055340788,
+                "icono" => "lighthouse-no.png",
+            ],
+        ];
+
+        $data = [
+            "coordenadas" => $luminaria,
+        ];
+
+        return $this->response->setJSON($data);
+
     }
 }
