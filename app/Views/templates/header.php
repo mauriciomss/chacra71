a0ffad427e798a740c0859e4d166a32e8027b110
mauriciomss <mauriciomss@gmail.com>
2023-11-14 09:33:55 -0300

    commit 14-11-2023 09:33:55

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -87,7 +87,7 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
+            <a href="." class="brand-link">
                 <img src="<?=base_url('assets/dist/img/logo.png');?>" alt="Logo" class="brand-image img-circle elevation-3">
                 <span class="brand-text font-weight-light">Chacra 71</span>
             </a>
